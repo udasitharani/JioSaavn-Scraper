0f7773bdcb56346aeeeb2e437fd569ce583ad3b8
udasitharani <udasitharani@gmail.com>
2020-03-31 16:52:28 +0530

    Fixed a small issue

--- app/home_scraper.py
+++ app/home_scraper.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*- 
 import requests
 from bs4 import BeautifulSoup
+import sys
+reload(sys)
+sys.setdefaultencoding('utf9')
 
 BASE_URL = "https://www.jiosaavn.com"
 HOME_URL = "/"
